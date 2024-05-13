48afee2383795970c28b350e987822283be1cfbd
pilou <p-l.b@hotmail.com>
2024-05-13 10:43:46 +0200

    initialized project

---
+++ src/test/java/com/poec/projet_backend/ProjetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poec.projet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
