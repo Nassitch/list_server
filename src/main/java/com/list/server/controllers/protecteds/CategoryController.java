2f8fdab2f9500b1debdad527049f72c34a69160a
Nassime HARMACH <Nassitch@hotmail.fr>
2024-06-17 09:24:35 +0200

    Add all methodes routes for 'Category' entity.

---
+++ src/main/java/com/list/server/controllers/protecteds/CategoryController.java
@@ -0,0 +1,40 @@
+package com.list.server.controllers.protecteds;
+
+import com.list.server.domain.entities.Category;
+import com.list.server.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/v1/admin/category")
+@RequiredArgsConstructor
+@Component("protectedCategoryController")
+public class CategoryController {
+
+    private final CategoryService service;
+
+    @PostMapping("/create")
+    public Category create(@RequestBody Category category) {
+        return this.service.add(category);
+    }
+
+    @PutMapping("/update/{id}")
+    public Category update(@RequestBody Category category, @PathVariable("id") Long id) {
+        return this.service.edit(category, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Long id) {
+        try {
+            String categoryDeleted = service.remove(id);
+            return new ResponseEntity<>(categoryDeleted, HttpStatus.CREATED);
+        } catch (IllegalArgumentException exception) {
+            String errorMsg = "This id: '" + id + "' was not founded.";
+            return new ResponseEntity<>(errorMsg, HttpStatus.NOT_FOUND);
+        }
+    }
+}
