d754bbff4aa973efdff41c8641dfccd4bd7d51d6
Nassitch <Nassitch@hotmail.fr>
2024-07-21 22:05:47 +0200

    Fix 'CRUD' methodes for item entity.

--- src/main/java/com/list/server/services/ItemService.java
+++ src/main/java/com/list/server/services/ItemService.java
@@ -1,7 +1,11 @@
 package com.list.server.services;
 
+import com.list.server.domain.entities.Invoice;
 import com.list.server.domain.entities.Item;
+import com.list.server.domain.entities.Shop;
+import com.list.server.repositories.InvoiceRepository;
 import com.list.server.repositories.ItemRepository;
+import com.list.server.repositories.ShopRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -14,6 +18,9 @@ import java.util.List;
 public class ItemService {
 
     private final ItemRepository repository;
+
+    private final ShopRepository shopRepository;
+    private final InvoiceRepository invoiceRepository;
 
     public List<Item> getAll() {
         return this.repository.findAll();
@@ -42,12 +49,24 @@ public class ItemService {
         return this.repository.save(itemEdited);
     }
 
-    public String remove(Long id) {
-        if (repository.existsById(id)) {
-            repository.deleteById(id);
-            return "id: " + id;
-        } else {
-            throw new IllegalArgumentException("This id: '" + id + "' was not founded.");
+    public void remove(Long id) {
+        Item item = getById(id);
+
+        List<Shop> shops = shopRepository.findShopsContainingItem(id);
+        for (Shop shop : shops) {
+            shop.getItems().remove(item);
+            shopRepository.save(shop);
         }
+
+        List<Invoice> invoices = invoiceRepository.findInvoicesContainingItem(id);
+        for (Invoice invoice : invoices) {
+            Shop shop = invoice.getShop();
+            if (shop != null && shop.getItems().contains(item)) {
+                shop.getItems().remove(item);
+                shopRepository.save(shop);
+            }
+        }
+
+        repository.delete(item);
     }
 }
